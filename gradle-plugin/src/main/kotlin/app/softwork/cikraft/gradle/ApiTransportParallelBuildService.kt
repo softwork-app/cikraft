bea3c65c9189167f43d247534dcae7c11d853fd2
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2026-08-27 08:00:21 +0000

    WIP (#158)

---
+++ gradle-plugin/src/main/kotlin/app/softwork/cikraft/gradle/ApiTransportParallelBuildService.kt
@@ -0,0 +1,6 @@
+package app.softwork.cikraft.gradle
+
+import org.gradle.api.services.BuildService
+import org.gradle.api.services.BuildServiceParameters
+
+interface ApiTransportParallelBuildService : BuildService<BuildServiceParameters.None>
